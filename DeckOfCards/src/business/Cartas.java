6c550171c1d9ce58eb1119a88624d060f5ffae45
juserrac <103602511+juserrac@users.noreply.github.com>
2023-09-13 21:46:09 -0300

    método distribuir cartas

--- DeckOfCards/src/business/Cartas.java
+++ DeckOfCards/src/business/Cartas.java
@@ -1,11 +1,13 @@
 package business;
-import org.json.*;
 
+import org.json.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
+import java.util.ArrayList;
 
 
 
@@ -26,6 +28,31 @@ public class Cartas {
 
 	        JSONObject jsonObject = new JSONObject(response.toString());
 	        return jsonObject.getString("deck_id");
+	    }
+	 
+	 private static List<String> distribuirCartas(String deckId, String jogador, int numCartas) throws IOException {
+	        URL url = new URL("https://deckofcardsapi.com/api/deck/" + deckId + "/draw/?count=" + numCartas);
+	        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+	        connection.setRequestMethod("GET");
+
+	        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+	        StringBuilder response = new StringBuilder();
+	        String line;
+	        while ((line = reader.readLine()) != null) {
+	            response.append(line);
+	        }
+	        reader.close();
+
+	        JSONObject jsonObject = new JSONObject(response.toString());
+	        JSONArray cardsArray = jsonObject.getJSONArray("cards");
+
+	        List<String> cartas = new ArrayList<>();
+	        for (int i = 0; i < cardsArray.length(); i++) {
+	            JSONObject cardObject = cardsArray.getJSONObject(i);
+	            cartas.add(cardObject.getString("code"));
+	        }
+
+	        return cartas;
 	    }
 	 
 	 
