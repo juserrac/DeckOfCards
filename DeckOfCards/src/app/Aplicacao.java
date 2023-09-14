17537da5d0251d9da146aa6d05bfe6ce767f77df
juserrac <103602511+juserrac@users.noreply.github.com>
2023-09-13 23:28:35 -0300

    criação da classe aplicação para acesso e execução dos métodos

--- DeckOfCards/src/app/Aplicacao.java
+++ DeckOfCards/src/app/Aplicacao.java
@@ -1,10 +1,42 @@
 package app;
 
+import org.json.*;
+import business.Cartas;
+import java.io.IOException;
+import java.util.List;
+
+
 public class Aplicacao {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        try {
+            String deckId = Cartas.criarNovoBaralho();
+            List<String> cartasAlan = Cartas.distribuirCartas(deckId, "Alan", 11);
+            List<String> cartasBruno = Cartas.distribuirCartas(deckId, "Bruno", 11);
 
-	}
+            Cartas.organizarMao(cartasAlan);
+            Cartas.organizarMao(cartasBruno);
 
-}
+            boolean sequenciaAlan = Cartas.temSequencia(cartasAlan);
+            boolean sequenciaBruno = Cartas.temSequencia(cartasBruno);
+
+            JSONObject resultado = Cartas.criarJSONMao(cartasAlan, cartasBruno);
+            resultado.put("tem_sequencia_alan", sequenciaAlan);
+            resultado.put("tem_sequencia_bruno", sequenciaBruno);
+
+            String vencedor = "empate";
+            if (sequenciaAlan && !sequenciaBruno) {
+                vencedor = "alan";
+            } else if (!sequenciaAlan && sequenciaBruno) {
+                vencedor = "bruno";
+            }
+
+            resultado.put("vencedor", vencedor);
+
+            System.out.println(resultado.toString(2));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+} 
+
