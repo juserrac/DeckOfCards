8f7b1059009265b0e63be9caea84b9d411aa8d81
juserrac <103602511+juserrac@users.noreply.github.com>
2023-09-13 23:00:08 -0300

    organização das cartas na mão e verificação de existencia de sequencia

--- DeckOfCards/src/business/Cartas.java
+++ DeckOfCards/src/business/Cartas.java
@@ -8,6 +8,10 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 
 
 
@@ -54,6 +58,74 @@ public class Cartas {
 
 	        return cartas;
 	    }
+	 
+	 
+	 private static int obterValorCarta(String carta) {
+	        String valor = carta.substring(0, carta.length() - 1);
+	        switch (valor) {
+	            case "ACE":
+	                return 1;
+	            case "KING":
+	                return 13;
+	            case "QUEEN":
+	                return 12;
+	            case "JACK":
+	                return 11;
+	            default:
+	                return Integer.parseInt(valor);
+	        }
+	    }
+	 
+	 private static void organizarMao(List<String> cartas) {
+		    for (int i = 0; i < cartas.size() - 1; i++) {
+		        for (int j = 0; j < cartas.size() - i - 1; j++) {
+		            String carta1 = cartas.get(j);
+		            String carta2 = cartas.get(j + 1);
+
+		            int valor1 = obterValorCarta(carta1);
+		            int valor2 = obterValorCarta(carta2);
+
+		            if (valor1 > valor2) {
+		                Collections.swap(cartas, j, j + 1);
+		                
+		            } else if (valor1 == valor2) {
+		                String naipe1 = carta1.substring(carta1.length() - 1);
+		                String naipe2 = carta2.substring(carta2.length() - 1);
+		                if (naipe1.compareTo(naipe2) > 0) {
+		                    Collections.swap(cartas, j, j + 1);
+		                }
+		            }
+		        }
+		    }
+		}	 
+
+	    
+	 private static boolean temSequencia(List<String> cartas) {
+		    Map<String, Integer> contagemNaipe = new HashMap<>();
+		    int sequenciaAtual = 1;
+
+		    for (int i = 0; i < cartas.size() - 1; i++) {
+		        String cartaAtual = cartas.get(i);
+		        String proximaCarta = cartas.get(i + 1);
+
+		        int valorAtual = obterValorCarta(cartaAtual);
+		        int valorProximaCarta = obterValorCarta(proximaCarta);
+		        String naipeAtual = cartaAtual.substring(cartaAtual.length() - 1);
+		        String naipeProximaCarta = proximaCarta.substring(proximaCarta.length() - 1);
+
+		        if (naipeAtual.equals(naipeProximaCarta) && valorProximaCarta == valorAtual + 1) {
+		            sequenciaAtual++;
+		        } else {
+		            sequenciaAtual = 1;
+		        }
+
+		        if (sequenciaAtual >= 3) {
+		            return true;
+		        }
+		    }
+
+		    return false;
+		}
 	 
 	 
 
